6ec91e7f084d89e58e14374804e1099123655381
Kumanan <kumanan1731@gmail.com>
2025-04-22 21:35:16 +0800

    add backend for hugging face api

--- index.js
+++ index.js
@@ -111,28 +111,28 @@ app.delete('/recipes/:id', async (req, res) => {
 });
 
 // Hugging Face AI
-// app.post('/generate-recipe', async (req, res) => {
-// 	const ingredientsArr = req.body.ingredients;
-// 	const ingredientsString = ingredientsArr.join(', ');
+app.post('/generate-recipe', async (req, res) => {
+	const ingredientsArr = req.body.ingredients;
+	const ingredientsString = ingredientsArr.join(', ');
 
-// 	try {
-// 		const response = await hf.chatCompletion({
-// 			model: 'mistralai/Mixtral-8x7B-Instruct-v0.1',
-// 			messages: [
-// 				{ role: 'system', content: SYSTEM_PROMPT },
-// 				{
-// 					role: 'user',
-// 					content: `I have ${ingredientsString}. Please give me a recipe you'd recommend I make!`,
-// 				},
-// 			],
-// 			max_tokens: 1024,
-// 		});
-// 		res.status(200).json({ recipe: response.choices[0].message.content });
-// 	} catch (err) {
-// 		console.error(err.message);
-// 		res.status(500).json({ error: 'Failed to generate recipe' });
-// 	}
-// });
+	try {
+		const response = await hf.chatCompletion({
+			model: 'mistralai/Mixtral-8x7B-Instruct-v0.1',
+			messages: [
+				{ role: 'system', content: SYSTEM_PROMPT },
+				{
+					role: 'user',
+					content: `I have ${ingredientsString}. Please give me a recipe you'd recommend I make!`,
+				},
+			],
+			max_tokens: 1024,
+		});
+		res.status(200).json({ recipe: response.choices[0].message.content });
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).json({ error: 'Failed to generate recipe' });
+	}
+});
 
 // toggle likes
 app.post('/recipes/:id/toggle-like', async (req, res) => {
