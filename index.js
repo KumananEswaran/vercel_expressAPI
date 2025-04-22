f6c056a4c134a3bbabb906a88adc3ffc886ea06b
Kumanan <kumanan1731@gmail.com>
2025-04-22 16:30:54 +0800

    testing

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import pg from 'pg';
-import { HfInference } from '@huggingface/inference';
+// import { HfInference } from '@huggingface/inference';
 
 app.use(cors());
 app.use(express.json());
 
-const SYSTEM_PROMPT = `You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients. You don't need to use every ingredient they mention in your recipe. The recipe can include additional ingredients they didn't mention, but try not to include too many ingredients. Format your response in markdown to make it easire to render to a web page`;
+// const SYSTEM_PROMPT = `You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients. You don't need to use every ingredient they mention in your recipe. The recipe can include additional ingredients they didn't mention, but try not to include too many ingredients. Format your response in markdown to make it easire to render to a web page`;
 
-const hf = new HfInference(process.env.HF_ACCESS_TOKEN);
+// const hf = new HfInference(process.env.HF_ACCESS_TOKEN);
 
 const { Pool } = pg;
 
@@ -111,28 +111,28 @@ app.delete('/recipes/:id', async (req, res) => {
 });
 
 // Hugging Face AI
-app.post('/generate-recipe', async (req, res) => {
-	const ingredientsArr = req.body.ingredients;
-	const ingredientsString = ingredientsArr.join(', ');
+// app.post('/generate-recipe', async (req, res) => {
+// 	const ingredientsArr = req.body.ingredients;
+// 	const ingredientsString = ingredientsArr.join(', ');
 
-	try {
-		const response = await hf.chatCompletion({
-			model: 'mistralai/Mixtral-8x7B-Instruct-v0.1',
-			messages: [
-				{ role: 'system', content: SYSTEM_PROMPT },
-				{
-					role: 'user',
-					content: `I have ${ingredientsString}. Please give me a recipe you'd recommend I make!`,
-				},
-			],
-			max_tokens: 1024,
-		});
-		res.status(200).json({ recipe: response.choices[0].message.content });
-	} catch (err) {
-		console.error(err.message);
-		res.status(500).json({ error: 'Failed to generate recipe' });
-	}
-});
+// 	try {
+// 		const response = await hf.chatCompletion({
+// 			model: 'mistralai/Mixtral-8x7B-Instruct-v0.1',
+// 			messages: [
+// 				{ role: 'system', content: SYSTEM_PROMPT },
+// 				{
+// 					role: 'user',
+// 					content: `I have ${ingredientsString}. Please give me a recipe you'd recommend I make!`,
+// 				},
+// 			],
+// 			max_tokens: 1024,
+// 		});
+// 		res.status(200).json({ recipe: response.choices[0].message.content });
+// 	} catch (err) {
+// 		console.error(err.message);
+// 		res.status(500).json({ error: 'Failed to generate recipe' });
+// 	}
+// });
 
 // toggle likes
 app.post('/recipes/:id/toggle-like', async (req, res) => {
